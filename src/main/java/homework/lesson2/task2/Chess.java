1e6cf8e936c2176d5b8206036cbcfeeb4e387296
krasnenkov <krasolee@gmail.com>
2021-05-10 19:53:08 +0300

    Добавлена 1 задача. Доделана 2 задача

--- src/main/java/homework/lesson2/task2/Chess.java
+++ src/main/java/homework/lesson2/task2/Chess.java
@@ -1,18 +1,59 @@
 package homework.lesson2.task2;
 
 public class Chess {
+
+    void chessFor(int[][] array) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    void chessWhile(int[][] array) {
+        int i = 0;
+        int j = 0;
+        while (i < 8) {
+            while (j < 8) {
+                System.out.print(array[i][j] + " ");
+                j++;
+            }
+            System.out.println();
+            i++;
+            j = 0;
+        }
+    }
+
+    void chessForEach(int[][] array) {
+        for (int[] i : array) {
+            for (int j : i) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 
         int array[][] = new int[8][8];
-        array[0][0] = 0;
-        for(int i=0; i<8; i++){
-            System.out.println();
-            for (int j=0; j<8; j++ ) {
-                if (((i%2 == 0) && (j%2!=0)) || ((j%2 ==0) && (i%2!=0))){
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (((i % 2 == 0) && (j % 2 != 0)) || ((j % 2 == 0) && (i % 2 != 0))) {
                     array[i][j] = 1;
                 }
-                System.out.print( array[i][j]+" ");
             }
         }
+        System.out.println("Вывод чере for:");
+        new Chess().chessFor(array);
+        System.out.println();
+
+        System.out.println("Вывод через ForEach:");
+        new Chess().chessForEach(array);
+        System.out.println();
+
+        System.out.println("Вывод через while:");
+        new Chess().chessWhile(array);
+        System.out.println();
     }
 }
